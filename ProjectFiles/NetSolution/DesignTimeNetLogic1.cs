9cbe5f8bc154f3c3bef5c0f186f1c5e91e0e4bf6
Tjoerge <tjoerge@rockwellautomation.com>
2022-12-21 15:02:46 +0100

    Addition

---
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -0,0 +1,26 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.DataLogger;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CommunicationDriver;
+using FTOptix.NetLogic;
+using FTOptix.Core;
+#endregion
+
+public class DesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
